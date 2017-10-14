6083438a589d7aa36dee34314018e70e0e809fa6
Yury Bilenkis <adm@bilenkis.ru>
2017-10-14 20:41:37 +0300

    fix adding group if does not exists

--- root/sbin/boot.sh
+++ root/sbin/boot.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-addgroup -g $PGID syncthing
-adduser -s /bin/false -u $PUID -G syncthing syncthing
+# this need if group already exists
+if [[ `getent group $PGID` ]]; then
+	GROUP_NAME=$(getent group $PGID | cut -d: -f1)
+else
+	GROUP_NAME="syncthing"
+	addgroup -g $PGID $GROUP_NAME
+fi
+
+adduser -D -s /bin/false -u $PUID -G $GROUP_NAME syncthing
 
 chown -R syncthing:syncthing /home/syncthing || true
 
